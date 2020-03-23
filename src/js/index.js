d74341e8d58d7e0025ea3a5ebd4cd0e555a57fc3
EricGarza <eric.garza13@gmail.com>
2020-03-23 16:59:12 -0700

    Implement search

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
-import num from './test';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-console.log(`I imported ${num} from another module!`);
+/** Global state of the app
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes
+ */
+const state = { };
+
+const controlSearch = async () => {
+  const query = searchView.getInput();
+
+  if (query) {
+    state.search = new Search(query);
+    searchView.clearInput();
+    searchView.clearResults();
+    await state.search.getResults();
+    searchView.renderResults(state.search.result);
+  }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
