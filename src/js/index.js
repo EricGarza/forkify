d80e708fc3df06f7e1dbbf39d9d7a82cf64d58a1
EricGarza <eric.garza13@gmail.com>
2020-06-15 20:42:16 -0700

    Add shopping list functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -11,6 +13,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
  *  - Liked recipes
  */
 const state = { };
+window.state = state;
 
 const controlSearch = async () => {
   const query = searchView.getInput();
@@ -72,6 +75,27 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+const controlList = () => {
+  if (!state.list) state.list = new List();
+
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    state.list.deleteItem(id);
+    listView.deleteItem(id);
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
   // match element that has css class of .btn-decrease or any child that does
@@ -81,7 +105,11 @@ elements.recipe.addEventListener('click', e => {
     }
   } else if (e.target.matches('.btn-increase, .btn-increase *')) {
     state.recipe.updateServings('inc');
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
 
   recipeView.updateServingsIngredients(state.recipe);
 });
+
+window.l = new List();
